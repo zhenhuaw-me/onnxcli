ac6922d66d8cc3741c9e13d8b0a0f267cf7f7c80
黎明灰烬 (王振华 Zhenhua WANG) <i@jackwish.net>
2021-12-24 10:37:30 +0000

    draw: update style

--- onnxcli/draw.py
+++ onnxcli/draw.py
@@ -2,8 +2,7 @@ import logging
 import tempfile
 import subprocess
 import shlex
-import re
-from onnxcli.common import SubCmd
+from onnxcli.common import SubCmd, dtype, shape
 
 logger = logging.getLogger('onnxcli')
 
@@ -27,27 +26,8 @@ class DrawCmd(SubCmd):
 
     def run(self, args):
         logger.info("Running <Graph Drawing> on model {}".format(args.input_path))
-        import onnx
 
-        def gen_graphviz(input_path):
-            m = onnx.load_model(input_path)
-            dot_str = "digraph graphname {\n"
-            for node in m.graph.node:
-                output_name = node.name
-                color = ""
-                if node.op_type.startswith("_"):
-                    color = ' color="yellow"'
-                if node.op_type == "CELL":
-                    color = ' color="red"'
-                dot_str += '"{}" [label="{},{}"{}];\n'.format(output_name, node.op_type, node.name, color)
-                for input_name in node.input:
-                    parts = input_name.split(":")
-                    input_name = re.sub(r"^\^", "", parts[0])
-                    dot_str += '  "{}" -> "{}";\n'.format(input_name, output_name)
-            dot_str += "}\n"
-            return dot_str
-
-        dot_str = gen_graphviz(args.input_path)
+        dot_str = self.gen_graphviz_str(args.input_path)
         if args.type == 'dot':
             with open(args.output_path, 'w') as f:
                 f.write(dot_str)
@@ -57,9 +37,44 @@ class DrawCmd(SubCmd):
                 tmpfp.flush()
                 tmpfp.seek(0)
                 dot_cmd = 'dot -T{} {} -o {}'.format(args.type, tmpfp.name, args.output_path)
-                logger.debug("Running {}".format(dot_cmd))
+                logger.debug("Invoking CMD {}".format(dot_cmd))
                 try:
                     subprocess.call(shlex.split(dot_cmd))
                 except OSError:
                     raise OSError("dot is not installed. Please install graphviz and try again.")
-        logging.info("Drawing graph is saved to {}".format(args.output_path))
+        logger.info("Drawing graph is saved to {}".format(args.output_path))
+
+    def gen_graphviz_str(self, input_path):
+        logger.debug("Generating graphviz string from {}".format(input_path))
+        import onnx
+
+        m = onnx.load_model(input_path)
+        dot_str = "digraph onnxcli {\n"
+
+        # handle chars that are key in graphviz
+        def fixname(name):
+            return name.replace('\\', '\\\\').replace(':', '\\:')
+
+        node_names = set()
+        for node in m.graph.node:
+            nname = fixname(node.name)
+            node_names.add(nname)
+            dot_str += '"{}" [label="{}\\n<{}>" fonstsize=16 shape=ellipse];\n'.format(nname, nname, node.op_type)
+            for iname in node.input:
+                dot_str += '  "{}" -> "{}";\n'.format(fixname(iname), nname)
+
+        # tensors
+        for tensor in m.graph.initializer:
+            tname = fixname(tensor.name)
+            dot_str += '"{}" [label="{}\\n{}, {}" fonstsize=10 style=rounded shape=rect];\n'.format(
+                tname, tname, dtype(tensor.data_type), tensor.dims
+            )
+        for tensor in m.graph.value_info:
+            tname = fixname(tensor.name)
+            if tname not in node_names:
+                dot_str += '"{}" [label="{}\\n{}, {}" fonstsize=10 shape=box];\n'.format(
+                    tname, tname, dtype(tensor.type.tensor_type.elem_type), shape(tensor.type.tensor_type.shape)
+                )
+
+        dot_str += "}\n"
+        return dot_str
