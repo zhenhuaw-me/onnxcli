8d288688f44ad4ac002b8b641b90f5cf44465131
黎明灰烬 (王振华 Zhenhua WANG) <i@jackwish.net>
2022-01-10 12:04:28 +0000

    inspect: print input and output too

--- onnxcli/inspect.py
+++ onnxcli/inspect.py
@@ -77,137 +77,161 @@ class InspectCmd(SubCmd):
             logger.warn("Failed to check model {}, statistic could be inaccurate!".format(args.input_path))
         m = onnx.load_model(args.input_path)
         g = m.graph
-
-        self.print_basic(args, g)
+        printed_any = False
 
         if args.meta:
-            self.print_meta(m)
+            print_meta(m)
+            printed_any = True
 
         if args.node:
-            self.print_nodes(g, args.indices, args.names, args.detail)
+            print_nodes(g, args.indices, args.names, args.detail)
+            printed_any = True
 
         if args.tensor:
-            self.print_tensor(g, args.indices, args.names, args.detail)
+            print_tensor(g, args.indices, args.names, args.detail)
+            printed_any = True
 
-    def print_meta(self, m):
-        print("\nMeta information:")
-        print("-" * 80)
-        print("  IR Version: {}".format(m.ir_version))
-        print("  Opset Import: {}".format(m.opset_import))
-        print("  Producer name: {}".format(m.producer_name))
-        print("  Producer version: {}".format(m.producer_version))
-        print("  Domain: {}".format(m.domain))
-        print("  Doc string: {}".format(m.doc_string))
-        for i in m.metadata_props:
-            print("  meta.{} = {}", i.key, i.value)
+        if not printed_any:
+            print_basic(args, g)
 
-    def print_basic(self, args, g):
-        print("Inpect of model {}".format(args.input_path))
-        print("=" * 80)
-        print("  Graph name: {}".format(len(g.name)))
-        print("  Graph inputs: {}".format(len(g.input)))
-        print("  Graph outputs: {}".format(len(g.output)))
-        print("  Nodes in total: {}".format(len(g.node)))
-        print("  ValueInfo in total: {}".format(len(g.value_info)))
-        print("  Initializers in total: {}".format(len(g.initializer)))
-        print("  Sparse Initializers in total: {}".format(len(g.sparse_initializer)))
-        print("  Quantization in total: {}".format(len(g.quantization_annotation)))
 
-    def print_tensor(self, g, indices, names, detail):
-        print("\nTensor information:")
-        print("-" * 80)
+def print_meta(m):
+    print("Meta information:")
+    print("-" * 80)
+    print("  IR Version: {}".format(m.ir_version))
+    print("  Opset Import: {}".format(m.opset_import))
+    print("  Producer name: {}".format(m.producer_name))
+    print("  Producer version: {}".format(m.producer_version))
+    print("  Domain: {}".format(m.domain))
+    print("  Doc string: {}".format(m.doc_string))
+    for i in m.metadata_props:
+        print("  meta.{} = {}", i.key, i.value)
 
-        def print_value_info(t):
-            txt = "  ValueInfo \"{}\":".format(t.name)
-            txt += " type {},".format(dtype(t.type.tensor_type.elem_type))
-            txt += " shape {},".format(shape(t.type.tensor_type.shape))
-            print(txt)
 
-        def print_initializer(t, detail):
-            txt = "  Initializer \"{}\":".format(t.name)
-            txt += " type {},".format(dtype(t.data_type))
-            txt += " shape {},".format(t.dims)
-            print(txt)
-            if detail:
-                print("    float data: {}".format(t.float_data))
+def print_basic(g):
+    print("  Graph name: {}".format(len(g.name)))
+    print("  Graph inputs: {}".format(len(g.input)))
+    print("  Graph outputs: {}".format(len(g.output)))
+    print("  Nodes in total: {}".format(len(g.node)))
+    print("  ValueInfo in total: {}".format(len(g.value_info)))
+    print("  Initializers in total: {}".format(len(g.initializer)))
+    print("  Sparse Initializers in total: {}".format(len(g.sparse_initializer)))
+    print("  Quantization in total: {}".format(len(g.quantization_annotation)))
 
-        # print with indices
-        if len(indices) > 0:
-            for idx in indices:
-                if idx >= len(g.value_info) and idx >= len(g.initializer):
-                    raise ValueError(
-                        "indices {} out of range, value_info in total {}, initializer in total {}".format(
-                            idx, len(g.value_info), len(g.initializer)
-                        )
-                    )
+
+def print_tensor(g, indices, names, detail):
+    print("Tensor information:")
+    print("-" * 80)
+
+    def print_value_info(t):
+        txt = "  ValueInfo \"{}\":".format(t.name)
+        txt += " type {},".format(dtype(t.type.tensor_type.elem_type))
+        txt += " shape {},".format(shape(t.type.tensor_type.shape))
+        print(txt)
+
+    def print_initializer(t, detail):
+        txt = "  Initializer \"{}\":".format(t.name)
+        txt += " type {},".format(dtype(t.data_type))
+        txt += " shape {},".format(t.dims)
+        print(txt)
+        if detail:
+            print("    float data: {}".format(t.float_data))
+
+    # print with indices
+    if len(indices) > 0:
+        for idx in indices:
+            printed_any = False
+            if idx < len(g.value_info):
+                printed_any = True
                 print_value_info(g.value_info[idx])
+            if idx < len(g.initializer):
                 print_initializer(g.initializer[idx], detail)
-            return
+                printed_any = True
+            if idx < len(g.input):
+                print_value_info(g.input[idx])
+                printed_any = True
+            if idx < len(g.output):
+                print_value_info(g.output[idx])
+                printed_any = True
+            if not printed_any:
+                raise ValueError("indices {} out of range, check the total size of tensors")
+        return
 
-        # print with names
-        if len(names) > 0:
-            found_any = False
-            for name in names:
-                for i in g.value_info:
-                    if i.name == name:
-                        print_value_info(i)
-                        found_any = True
-                        break
-                for i in g.initializer:
-                    if i.name == name:
-                        print_initializer(i, detail)
-                        found_any = True
-                        break
-            if not found_any:
-                raise ValueError("No tensor found with name {}".format(name))
-            return
+    # print with names
+    if len(names) > 0:
+        printed_any = False
+        for name in names:
+            for i in g.value_info:
+                if i.name == name:
+                    print_value_info(i)
+                    printed_any = True
+                    break
+            for i in g.initializer:
+                if i.name == name:
+                    print_initializer(i, detail)
+                    printed_any = True
+                    break
+            for i in g.input:
+                if i.name == name:
+                    print_value_info(i)
+                    printed_any = True
+                    break
+            for i in g.output:
+                if i.name == name:
+                    print_value_info(i)
+                    printed_any = True
+                    break
+        if not printed_any:
+            raise ValueError("No tensor found with name {}".format(name))
+        return
 
-        # print all tensors
-        for t in g.value_info:
-            print_value_info(t)
-        for t in g.initializer:
-            print_initializer(t, False)
+    # print all tensors
+    for t in g.value_info:
+        print_value_info(t)
+    for t in g.initializer:
+        print_initializer(t, False)
 
-    def print_nodes(self, g, indices, names, detail):
-        print("\nNode information:")
-        print("-" * 80)
 
-        def print_node(n, detail):
-            txt = "  Node \"{}\":".format(n.name)
-            txt += " type \"{}\",".format(n.op_type)
-            txt += " inputs \"{}\",".format(n.input)
-            txt += " outputs \"{}\"".format(n.output)
-            print(txt)
-            if detail and len(n.attribute) > 0:
-                print("    attributes: {}".format(n.attribute))
+def print_nodes(g, indices, names, detail):
+    print("Node information:")
+    print("-" * 80)
 
-        # print with indices
-        if len(indices) > 0:
-            for idx in indices:
-                if idx >= len(g.node):
-                    raise ValueError("indices {} out of range, node in total {}".format(idx, len(g.node)))
-                print_node(g.node[idx], detail)
-            return
+    def print_node(n, detail):
+        txt = "  Node \"{}\":".format(n.name)
+        txt += " type \"{}\",".format(n.op_type)
+        txt += " inputs \"{}\",".format(n.input)
+        txt += " outputs \"{}\"".format(n.output)
+        print(txt)
+        if detail and len(n.attribute) > 0:
+            print("    attributes: {}".format(n.attribute))
 
-        # print with names
-        if len(names) > 0:
-            found_any = False
-            for name in names:
-                for n in g.node:
-                    if n.name == name:
-                        print_node(n, detail)
-                        found_any = True
-                        break
-            if not found_any:
-                raise ValueError("No node found with name {}".format(name))
-            return
+    # print with indices
+    if len(indices) > 0:
+        for idx in indices:
+            if idx >= len(g.node):
+                raise ValueError("indices {} out of range, node in total {}".format(idx, len(g.node)))
+            print_node(g.node[idx], detail)
+        return
 
-        import collections
+    # print with names
+    if len(names) > 0:
+        found_any = False
+        for name in names:
+            for n in g.node:
+                if n.name == name:
+                    print_node(n, detail)
+                    found_any = True
+                    break
+        if not found_any:
+            raise ValueError("No node found with name {}".format(name))
+        return
 
-        ops = collections.Counter([node.op_type for node in g.node])
-        for op, count in ops.most_common():
-            print("  Node type \"{}\" has: {}".format(op, count))
+    import collections
 
-        print("-" * 80)
-        for node in g.node:
-            print_node(node, False)
+    ops = collections.Counter([node.op_type for node in g.node])
+    for op, count in ops.most_common():
+        print("  Node type \"{}\" has: {}".format(op, count))
+
+    print("-" * 80)
+    for node in g.node:
+        print_node(node, False)
