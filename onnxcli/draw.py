9ecd76e24f55a1cdf905a6baf40825efadae6f94
王振华 (Zhenhua WANG) <i@jackwish.net>
2022-01-25 17:15:48 +0800

    fix list concat issue

--- onnxcli/draw.py
+++ onnxcli/draw.py
@@ -91,7 +91,7 @@ class DrawCmd(SubCmd):
             dot_str += '"{}" [label="{}\\n{}, {}" fonstsize=10 style=rounded shape=rectangle];\n'.format(
                 tensor_key(tensor.name), fixname(tensor.name), dtype(tensor.data_type), tensor.dims
             )
-        all_value_info = m.graph.value_info + m.graph.input + m.graph.output
+        all_value_info = list(m.graph.value_info) + list(m.graph.input) + list(m.graph.output)
         for tensor in all_value_info:
             if tensor.name in tensor_names:
                 tensor_names.remove(tensor.name)
@@ -102,10 +102,12 @@ class DrawCmd(SubCmd):
                     shape(tensor.type.tensor_type.shape),
                 )
 
-        # the tensors that are not in graph.initializer nor graph.value_info
-        # i.e. they only have names
-        for tname in tensor_names:
-            dot_str += '"{}" [label="{}" fonstsize=10 shape=rectangle];\n'.format(tensor_key(tname), fixname(tname))
+        if len(tensor_names) != 0:
+            # the tensors that are not in graph.initializer nor graph.value_info
+            # i.e. they only have names
+            logger.warning("There are tensors that only have name in the graph. Suggest to run with `onnx infershape`.")
+            for tname in tensor_names:
+                dot_str += '"{}" [label="{}" fonstsize=10 shape=rectangle];\n'.format(tensor_key(tname), fixname(tname))
 
         dot_str += "}\n"
         return dot_str
